8501615e425d875f6fa3a890975f78b4c88ee192
tcs <vladgalstyan13@gmail.com>
2026-06-20 09:38:34 -0700

    hi

--- undecided/NAME/src/Homepage.jsx
+++ undecided/NAME/src/Homepage.jsx
@@ -20,21 +20,21 @@ function Homepage() {
         <h2>Projects</h2>
       </div>
       <div id="cardholder">
-        <div className="card tinted-glass">
+        <div className="project-card card tinted-glass">
           <h3>Tic Tac Toe</h3>
           <p>
             A game is played in which two players take turns drawing three O's
             or three X's in a row, column or diagonal in nine coordinate spaces.
           </p>
         </div>
-        <div className="card tinted-glass">
+        <div className="project-card card tinted-glass">
           <h3>Flappy Bird</h3>
           <p>
             A game where you have to click the screen in order to help a bird
             survive past certain obstacles by going through an opening.
           </p>
         </div>
-        <div className="card tinted-glass">
+        <div className="project-card card tinted-glass">
           <h3>Stock Simulator</h3>
           <p>
             A game where you can learn the basics about investing and simulate
@@ -46,18 +46,21 @@ function Homepage() {
       <div>
         <h2>About me</h2>
       </div>
-      <div className="tinted-glass"  id="aboutme">
-        I am a rising junior at Torrance High School and I want to pursue a
-        career in STEM. I am a co-founder of the Robotics Club at our school,
-        and we are aiming to compete in VEX Robotics VRC competitions this
-        upcoming year. I have competed in various math competitions over middle
-        school and high school, and I have competed in the LACOE Math Field Day
-        for 4 years, in which I have got 2 bronze medals as a freshman and 2
-        silver medals as a sophomore. I recently started doing science olympiad,
-        and we are aiming to go to invitational competions in the winter/spring.
-        I am playing tennis for my school as well, and I made varisty my
-        sophomore year. I have become the junior captain of the tennis team and
-        am excited to meet new people.
+
+      <div className="card tinted-glass" id="aboutme">
+        <p>
+          I am a rising junior at Torrance High School and I want to pursue a
+          career in STEM. I am a co-founder of the Robotics Club at our school,
+          and we are aiming to compete in VEX Robotics VRC competitions this
+          upcoming year. I have competed in various math competitions over
+          middle school and high school, and I have competed in the LACOE Math
+          Field Day for 4 years, in which I have got 2 bronze medals as a
+          freshman and 2 silver medals as a sophomore. I recently started doing
+          science olympiad, and we are aiming to go to invitational competions
+          in the winter/spring. I am playing tennis for my school as well, and I
+          made varisty my sophomore year. I have become the junior captain of
+          the tennis team and am excited to meet new people.
+        </p>
       </div>
 
       {/*  FIGMA */}
